0a8601011792b92e4c5fa5446d205ebaa4a02982
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-11-25 18:39:47 -0300

    Adding style

--- project-reactjs/src/App.js
+++ project-reactjs/src/App.js
@@ -1,16 +1,28 @@
 
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
-    <div className="contaqiner ">
+    <div className="container ">
       <header className="title">
         <h1> Buscador de CEP</h1>
       </header>
 
       <div className="containerUnput">
         <input type="text" typeof="text" placeholder="Digite um CEP..." />
-        <button className="buttonSearch">Procurar</button>
+        <button className="buttonSearch">Procurar
+          <FiSearch size={25} color="#fff" />
+        </button>
       </div>
+
+      <main className='main'>
+        <h2>CEP</h2>
+        <span>Rua Teste</span>
+        <span>Bairro</span>
+        <span>Cidade - SC</span>
+      </main>
+
     </div>
   );
 }
