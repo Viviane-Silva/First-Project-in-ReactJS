39a2ddfb961538128fae9f0ed5e83f79821f5c14
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-11-25 18:26:51 -0300

    Starting

---
+++ project-reactjs/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="contaqiner ">
+      <header className="title">
+        <h1> Buscador de CEP</h1>
+      </header>
+
+      <div className="containerUnput">
+        <input type="text" typeof="text" placeholder="Digite um CEP..." />
+        <button className="buttonSearch">Procurar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
